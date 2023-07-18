be4704e805bfd238783979e1a628357fd4b414d2
David Poza Suárez <david.poza-suarez@t-systems.com>
2023-07-18 18:04:54 +0200

    refresh token shoudn't include redirect to the root

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -110,8 +110,6 @@ export class AuthService {
               };
               localStorage.setItem('user', JSON.stringify(this.user));
             });
-
-          this.router.navigate(['/']);
         })
       )
   }
